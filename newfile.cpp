95fba8bb8fc4bcb7c84065abfe92415eca3abe28
Saanchita V <106515879+SaanchitaV@users.noreply.github.com>
2023-02-19 20:17:24 +0530

    Update newfile.cpp

--- newfile.cpp
+++ newfile.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "New File\n";
   cout << "PES1UG20CS364\n";
-  cout << "Pipeline Execution Example\n";
+  cout << "Pipeline Execution\n";
   return 0;
 }
